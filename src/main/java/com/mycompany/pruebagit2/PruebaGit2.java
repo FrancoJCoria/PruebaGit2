93acee9b2023c05d17e4cab4df8252eba7b1e64c
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-08-28 14:12:25 -0300

    Comentario agregado en la rama main

---
+++ src/main/java/com/mycompany/pruebagit2/PruebaGit2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagit2;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class PruebaGit2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Comentario creado en la rama main");
+    }
+}
